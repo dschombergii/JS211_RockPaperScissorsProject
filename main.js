9d9472386358278f7435519a24711161c6665b08
dschombergii <d.schombergii@gmail.com>
2020-10-27 10:33:46 -0500

    added testing conditionals

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 // uses strict mode so strings are not coerced, variables are not hoisted, etc... 
 'use strict';
 
+
+
+// the function that will be called by the unit test below
+const rockPaperScissors = (hand1, hand2) => {
+
+  // Write code here
+  // Use the unit test to see what is expected
+
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
+
+  if ((hand1 == 'rock' && hand2 == 'rock') || (hand1 == 'paper' && hand2 == 'paper') || (hand1 == 'scissors' && hand2 == 'scissors')) {
+    return("It's a tie!")
+  } else if 
+    ((hand1 == 'rock' && hand2 == 'scissors') || (hand1 == 'paper' && hand2 == 'rock') || (hand1 == 'scissors' && hand2 == 'paper')) {
+    return("Hand one wins!")
+  } else if ((hand2 == 'rock' && hand1 == 'scissors') || (hand2 == 'paper' && hand1 == 'rock') || (hand2 == 'scissors' && hand1 == 'paper')) {
+    return("Hand two wins!")
+  } else {
+    return('Oh come on now, enter "rock", "paper", or "scissors" you goofy goober!')
+  }
+}
+
+
+
 // brings in the assert module for unit testing
 const assert = require('assert');
 // brings in the readline module to access the command line
@@ -11,13 +36,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
-
-  // Write code here
-  // Use the unit test to see what is expected
-
-}
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -31,12 +49,14 @@ function getPrompt() {
   });
 }
 
+
 // Unit Tests
 // You use them run the command: npm test main.js
 // to close them ctrl + C
 if (typeof describe === 'function') {
 
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built return the expected output.
+  // most are notes for human eyes to read, but essentially passes in inputs then compares 
+  //if the function you built return the expected output.
   describe('#rockPaperScissors()', () => {
     it('should detect a tie', () => {
       assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
@@ -58,5 +78,4 @@ if (typeof describe === 'function') {
 
   // always returns ask the user for another input
   getPrompt();
-
 }
